f2691d0ec74da76fb4df4f1cf3373c857ed9fc24
wejdenebens <wejdenebenslimane@gmail.com>
2020-09-05 08:46:59 +0100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,27 @@
+
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+const [data, setData] = useState({ listOfUSer : [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        "https://jsonplaceholder.typicode.com/users?query=redux"
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {data.listOfUSer.map(item => (
+        <li key={item.id}>
+          <a href={item.name}>{item.username}</a>
+        </li>
+      ))}
+    </ul>
+  );
+
